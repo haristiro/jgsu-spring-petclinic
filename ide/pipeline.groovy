3360a234215d7966fa44b2850ce7f9c8312a5fcb
Tiro <Haris.Tiro@heidelbergcement.com>
2021-11-01 20:19:15 +0100

    commiting groovy

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -17,9 +17,9 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                // }
+                // changed {
+                    emailext subject: ""Job '${JOB_NAME}' (${BUILD_NUMBER}) is waiting for input",
                         body: "Please go to ${BUILD_URL} and verify the build", 
                         attachLog: true, 
                         compressLog: true, 
